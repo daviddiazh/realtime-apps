8ecb6add75aa428dd12db21524f83cd8bd397965
daviddiazh <daviddiazlds1221@gmail.com>
2022-10-09 17:16:49 -0500

    Adding bussines logic to Populate Language on Server Side

--- populate-language/server/models/language-list.js
+++ populate-language/server/models/language-list.js
@@ -1,4 +1,50 @@
+const Language = require("./language");
 
 class LanguageList {
     
+    constructor(){
+        this.languages = [
+            new Language('Java'),
+            new Language('JavaScript'),
+            new Language('SQL')
+        ];
+    }
+
+    addLanguage ( name ) {
+        const newLanguage = new Language( name );
+        this.languages.push( newLanguage );
+
+        return this.languages;
+    }
+
+    removeLanguage ( id ) {
+        this.languages.filter( language => language.id !== id );
+    }
+
+    getLanguages () {
+        return this.languages;
+    }
+
+    increaseVotes(id) {
+        this.languages = this.languages.map( language => {
+            if(language.id === id) {
+                language.votes += 1;
+            }
+
+            return language;
+        })
+    }
+
+    changeLanguageName ( id, newName ){
+        this.languages.map( language => {
+            if(language.id === id){
+                language.name = newName;
+            }
+
+            return language;
+        })
+    }
+
 }
+
+module.exports = LanguageList;
--- populate-language/server/models/language.js
+++ populate-language/server/models/language.js
@@ -1,4 +1,13 @@
+const { v4: uuidv4 } = require('uuid');
 
 class Language {
+
+    constructor( name ){
+        this.id = uuidv4();
+        this.name = name;
+        this.votes = 0;
+    }
     
 }
+
+module.exports = Language;
